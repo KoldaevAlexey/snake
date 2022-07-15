bf5b8f99f00d69bff0cb80f1985f0ab320f0c301
Randark <koldaev.alexey@gmail.com>
2022-07-15 22:52:24 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const canvas = document.querySelector("#playing-field");
+
+const cell = 16;
+
+const snake = {
+  x: 320,
+  y: 320,
+  moveX: cell,
+  moveY: 0,
+  tail: [],
+  maxTail: 2,
+};
+
+const food = {
+  x: 160,
+  y: 160,
+};
+
+function foodRender() {
+  //todo
+}
+
+function snakeRender() {
+  //todo
+}
+
+function snakeEat() {
+  //todo
+}
+
+function scoreUpdate() {
+  //todo
+}
+
+function gameOver() {
+  //todo
+}
+
+function snakeCanvasBorder() {
+  //todo
+}
