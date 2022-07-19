aa69367873df203a467c09effd4ea243b1ed3858
Randark <koldaev.alexey@gmail.com>
2022-07-19 20:51:02 +0300

    v1.0

--- script.js
+++ script.js
@@ -7,123 +7,128 @@ const canvas = document.querySelector("#playing-field"),
   btnConfirm = resultModal.querySelector(".btn-confirm"),
   inputName = resultModal.querySelector(".modal_input");
 
+let currentScore = 0,
+  players,
+  pausedGame = false;
+
 const config = {
   grid: 16,
   count: 0,
+  frameLimiter: 4,
 };
-
-let currentScore = 0;
-let players;
-
-if (!localStorage.getItem("players")) {
-  players = [];
-} else {
-  players = JSON.parse(localStorage.getItem("players"));
-
-  let jsonPlayers = JSON.stringify(players);
-  localStorage.setItem("players", jsonPlayers);
-
-  players.sort((a, b) => {
-    return b.score - a.score;
-  });
-
-  for (let i = 0; i < players.length; i++) {
-    addRatingList(players[i].name, players[i].score);
-  }
-}
 
 const snake = {
   x: 160,
   y: 160,
   dx: config.grid,
   dy: 0,
-  cells: [],
-  maxCells: 4,
+  tail: [],
+  maxTail: 4,
+
+  reset() {
+    snake.x = 320;
+    snake.y = 320;
+    snake.dx = config.grid;
+    snake.dy = 0;
+    snake.tail = [];
+    snake.maxTail = 4;
+  },
+  eat() {
+    snake.maxTail++;
+  },
+  crossBorderCanvas() {
+    if (snake.x < 0) {
+      snake.x = canvas.width - config.grid;
+    } else if (snake.x >= canvas.width) {
+      snake.x = 0;
+    }
+    if (snake.y < 0) {
+      snake.y = canvas.height - config.grid;
+    } else if (snake.y >= canvas.height) {
+      snake.y = 0;
+    }
+  },
+  startMove() {
+    snake.x += snake.dx;
+    snake.y += snake.dy;
+  },
+  movementAnimation() {
+    snake.tail.unshift({ x: snake.x, y: snake.y });
+    if (snake.tail.length > snake.maxTail) {
+      snake.tail.pop();
+    }
+  },
 };
 
-const apple = {
-  x: 320,
-  y: 320,
-};
+const food = {
+  x: getRandomInt(0, 25) * config.grid,
+  y: getRandomInt(0, 25) * config.grid,
 
-let n = 4,
-  pausedCheck = false;
+  render() {
+    ctx.fillStyle = "aqua";
+    ctx.fillRect(food.x, food.y, config.grid - 1, config.grid - 1);
+  },
+  spawn() {
+    food.x = getRandomInt(0, 25) * config.grid;
+    food.y = getRandomInt(0, 25) * config.grid;
+  },
+};
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function loop() {
-  requestAnimationFrame(loop);
-
-  pausedCheck === true ? (n = 10000) : (n = 4);
-
-  if (++config.count < n) {
-    return;
+function canvasShowHide() {
+  if (canvas.classList.contains("show")) {
+    canvas.classList.add("hide");
+    canvas.classList.remove("show");
+  } else {
+    canvas.classList.add("show");
+    canvas.classList.remove("hide");
   }
+}
 
-  scoreLabel.textContent = `ОЧКИ: ${currentScore}`;
-  config.count = 0;
-
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  snake.x += snake.dx;
-  snake.y += snake.dy;
-
-  if (snake.x < 0) {
-    snake.x = canvas.width - config.grid;
-  } else if (snake.x >= canvas.width) {
-    snake.x = 0;
+function modalShowHide() {
+  if (resultModal.classList.contains("show")) {
+    resultModal.classList.add("hide");
+    resultModal.classList.remove("show");
+  } else {
+    resultModal.classList.add("show");
+    resultModal.classList.remove("hide");
   }
-  if (snake.y < 0) {
-    snake.y = canvas.height - config.grid;
-  } else if (snake.y >= canvas.height) {
-    snake.y = 0;
+}
+
+function addRatingList(name, score) {
+  if (players.length > 10) {
+    players.pop();
   }
+  let playerList = document.createElement("li");
+  document.querySelector(".rating").append(playerList);
+  playerList.textContent = `${name} : ${score}`;
+}
 
-  snake.cells.unshift({ x: snake.x, y: snake.y });
-  if (snake.cells.length > snake.maxCells) {
-    snake.cells.pop();
-  }
-  ctx.fillStyle = "aqua";
-  ctx.fillRect(apple.x, apple.y, config.grid - 1, config.grid - 1);
-  ctx.fillStyle = "lightgreen";
+function loadDataFromLocalStorage() {
+  if (!localStorage.getItem("players")) {
+    players = [];
+  } else {
+    players = JSON.parse(localStorage.getItem("players"));
 
-  snake.cells.forEach(function (cell, index) {
-    ctx.fillRect(cell.x, cell.y, config.grid - 1, config.grid - 1);
+    let jsonPlayers = JSON.stringify(players);
+    localStorage.setItem("players", jsonPlayers);
 
-    if (cell.x === apple.x && cell.y === apple.y) {
-      snake.maxCells++;
-      currentScore++;
+    sortPlayersScoreForRatingList();
 
-      apple.x = getRandomInt(0, 25) * config.grid;
-      apple.y = getRandomInt(0, 25) * config.grid;
+    for (let i = 0; i < players.length; i++) {
+      addRatingList(players[i].name, players[i].score);
     }
+  }
+}
 
-    for (let i = index + 1; i < snake.cells.length; i++) {
-      if (cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) {
-        snake.x = 320;
-        snake.y = 320;
-        snake.dx = config.grid;
-        snake.dy = 0;
-        snake.cells = [];
-        snake.maxCells = 4;
-
-        canvas.classList.add("hide");
-        canvas.classList.remove("show");
-
-        resultModal.classList.add("show");
-        resultModal.classList.remove("hide");
-
-        pausedCheck = true;
-
-        apple.x = getRandomInt(0, 25) * config.grid;
-        apple.y = getRandomInt(0, 25) * config.grid;
-      }
-    }
+function sortPlayersScoreForRatingList() {
+  players.sort((a, b) => {
+    return b.score - a.score;
   });
 }
-document.addEventListener("keydown", direction);
 
 function direction(e) {
   if (e.keyCode === 37 && snake.dx === 0) {
@@ -143,9 +148,56 @@ function direction(e) {
     snake.dx = 0;
   }
   if (e.keyCode === 32) {
-    pausedCheck ? (pausedCheck = false) : (pausedCheck = true);
+    pausedGame ? (pausedGame = false) : (pausedGame = true);
   }
 }
+
+loadDataFromLocalStorage();
+
+function game() {
+  requestAnimationFrame(game);
+
+  pausedGame === true
+    ? (config.frameLimiter = 10000)
+    : (config.frameLimiter = 4);
+
+  if (++config.count < config.frameLimiter) {
+    return;
+  }
+
+  scoreLabel.textContent = `SCORE: ${currentScore}`;
+  config.count = 0;
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  snake.startMove();
+  snake.crossBorderCanvas();
+  snake.movementAnimation();
+  food.render();
+
+  snake.tail.forEach(function (cell, index) {
+    ctx.fillStyle = "lightgreen";
+    ctx.fillRect(cell.x, cell.y, config.grid - 1, config.grid - 1);
+
+    if (cell.x === food.x && cell.y === food.y) {
+      snake.eat();
+      currentScore++;
+      food.spawn();
+    }
+
+    for (let i = index + 1; i < snake.tail.length; i++) {
+      if (cell.x === snake.tail[i].x && cell.y === snake.tail[i].y) {
+        snake.reset();
+        canvasShowHide();
+        modalShowHide();
+        pausedGame = true;
+        food.spawn();
+      }
+    }
+  });
+}
+
+document.addEventListener("keydown", direction);
 
 btnConfirm.addEventListener("click", () => {
   players.push({ name: inputName.value, score: currentScore });
@@ -153,17 +205,11 @@ btnConfirm.addEventListener("click", () => {
   let jsonPlayers = JSON.stringify(players);
   localStorage.setItem("players", jsonPlayers);
 
-  players.sort((a, b) => {
-    return b.score - a.score;
-  });
+  sortPlayersScoreForRatingList();
+  modalShowHide();
+  canvasShowHide();
 
-  resultModal.classList.add("hide");
-  resultModal.classList.remove("show");
-
-  canvas.classList.add("show");
-  canvas.classList.remove("hide");
-
-  pausedCheck = false;
+  pausedGame = false;
   currentScore = 0;
 
   document.querySelectorAll("li").forEach((item) => {
@@ -172,15 +218,7 @@ btnConfirm.addEventListener("click", () => {
   for (let i = 0; i < players.length; i++) {
     addRatingList(players[i].name, players[i].score);
   }
+  inputName.value = "";
 });
 
-function addRatingList(name, score) {
-  if (players.length > 10) {
-    players.pop();
-  }
-  let playerList = document.createElement("li");
-  document.querySelector(".rating").append(playerList);
-  playerList.textContent = `${name} : ${score}`;
-}
-
-requestAnimationFrame(loop);
+requestAnimationFrame(game);
